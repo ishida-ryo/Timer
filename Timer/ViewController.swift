92dc8979ce5cd9b78ce1aded35407802350d8cd4
石田陵 <ishidaryou@ishidaryounoMacBook-Pro.local>
2017-02-14 19:54:52 +0900

    タイマーアプリ作成

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -1,25 +1,51 @@
-//
-//  ViewController.swift
-//  Timer
-//
-//  Created by 石田陵 on 2017/02/14.
-//  Copyright © 2017年 ryo.ishida. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    var timer: Timer!
+    var timer_sec: Float = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+            }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    func updateTimer(timer: Timer){
+        self.timer_sec += 0.1
+        self.timerLabel.text = String(format: "%.1f", timer_sec)
+    }
 
+    @IBAction func startTimer(_ sender: Any) {
+        if self.timer == nil {
+        self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        } else {
+            self.timer.invalidate()
+            self.timer = nil            
+        }
+
+    }
+    
+    @IBAction func pauseTimer(_ sender: Any) {
+        if self.timer != nil {
+            self.timer.invalidate()
+            self.timer = nil
+        }
+    }
+    
+    @IBAction func resetTimer(_ sender: Any) {
+        self.timer_sec = 0
+        self.timerLabel.text = String(format: "%.1f", self.timer_sec)
+        
+        if self.timer != nil{
+            self.timer.invalidate()
+            self.timer = nil
+        }
+    }
+    
 }
 
